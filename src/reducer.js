92bab81ba3827496501fa573112e9fc7f3f5ff1f
Raul Tomescu <tomescu.raul@gmail.com>
2018-03-28 21:56:00 +0200

    Added mock todo

--- src/reducer.js
+++ src/reducer.js
@@ -1,3 +1,7 @@
-export function reducer(state = {}, action) {
-  return {};
-}
+import { combineReducers } from "redux";
+
+import { reducer as todoReducer } from "./scenes/App/redux/reducer";
+
+export const reducer = combinerReducers({
+  todoReducer
+});
---
+++ src/scenes/App/redux/reducer.js
@@ -0,0 +1,10 @@
+const initialState = [
+  {
+    text: "This is a todo",
+    isCompleted: false
+  }
+];
+
+export function reducer(state = initialState, action) {
+  return {};
+}
